d7812bbf0b62a2fe6fed0ecdfb76fc91f8f1e4eb
abvcghsdcxy <fr13ndxdxdxd@gmail.com>
2026-07-01 13:26:24 +0200

    update stuff

--- loader.lua
+++
@@ -1,5 +0,0 @@
---!grant RuntimeLoadstring
-rbxcli.loadstring(game:GetService("HttpService"):RequestAsync({
-    Method = "GET",
-    Url = "https://raw.githubusercontent.com/abvcghsdcxy/omg6767676767676767/refs/heads/main/main.lua"
-}).Body)()
